9770e6349b1ab4ee8f63586ac10ae27a4353e2e2
unknown <ganeshg1582000@gmail.com>
2023-08-26 19:12:07 +0530

    Updates

---
+++ precache-manifest.a0e021b32521bd6e65e3f053d77955b5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "975954285c1bf3f3a147",
+    "url": "/stopWatch/static/css/main.1dae5ed0.chunk.css"
+  },
+  {
+    "revision": "975954285c1bf3f3a147",
+    "url": "/stopWatch/static/js/main.0409ebf0.chunk.js"
+  },
+  {
+    "revision": "c1612eb38a36045931f0",
+    "url": "/stopWatch/static/js/runtime~main.2c6d16bc.js"
+  },
+  {
+    "revision": "65111a2944747c012b97",
+    "url": "/stopWatch/static/js/2.aef98978.chunk.js"
+  },
+  {
+    "revision": "cecf616dbfc830b280dc",
+    "url": "/stopWatch/static/js/3.d7f7bd74.chunk.js"
+  },
+  {
+    "revision": "97b26aa485ac2465267b00dcf9f84b4e",
+    "url": "/stopWatch/index.html"
+  }
+];
